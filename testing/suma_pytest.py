c7388bef8c5c75b495b6d9af4f77976430445c21
Cristian Nestor Carle <cristiancarle@gmail.com>
2025-04-21 05:41:42 -0300

    pytest

--- testing/suma_pytest.py
+++ testing/suma_pytest.py
@@ -5,3 +5,9 @@ def test_suma():
     assert sumar(2, 3) == 5
     assert sumar(-1, 1) == 0
     assert sumar(0, 0) == 0
+
+print("Ejecutando pruebas...")
+print("Pruebas de suma.py")
+print("Pruebas de la función sumar")
+print(test_suma())
+print("Pruebas completadas.") 
